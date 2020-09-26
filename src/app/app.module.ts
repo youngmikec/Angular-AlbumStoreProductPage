18dbaace894481a91872298b0c9f331a90c0de79
ozor michael chisom <michaelozor15@gmail.com>
2020-09-26 19:35:14 +0100

    update appRoutes array to be of type Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,10 +12,10 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 
 
-const appRoutes = [
-  {path: "products", component: ProductListComponent},
-  {path: "product/:id", component: ProductPageComponent},
-  {path: "", redirectTo: "products", pathMatch: 'full'}
+const appRoutes:Routes = [
+  {path: 'products', component: ProductListComponent},
+  {path: 'product/:id', component: ProductPageComponent},
+  {path: '', redirectTo: "products", pathMatch: 'full'}
 ];
 
 @NgModule({
